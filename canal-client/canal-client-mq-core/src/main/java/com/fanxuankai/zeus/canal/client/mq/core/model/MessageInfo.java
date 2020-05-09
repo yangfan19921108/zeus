44740343646fdc3e89e3ab23362917ce014156ba
fanxuankai <fan_xuankai@126.com>
2020-05-09 16:21:54 +0800

    clean

--- canal-client/canal-client-mq-core/src/main/java/com/fanxuankai/zeus/canal/client/mq/core/model/MessageInfo.java
+++ canal-client/canal-client-mq-core/src/main/java/com/fanxuankai/zeus/canal/client/mq/core/model/MessageInfo.java
@@ -1,7 +1,6 @@
 package com.fanxuankai.zeus.canal.client.mq.core.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 
 import java.util.List;
@@ -15,17 +14,5 @@ import java.util.List;
 @Getter
 public class MessageInfo {
     private final String routingKey;
-    private final List<Message> messages;
-
-    @Data
-    public static class Message {
-        /**
-         * 消息摘要
-         */
-        private String md5;
-        /**
-         * json 字符串
-         */
-        private String data;
-    }
+    private final List<String> messages;
 }
